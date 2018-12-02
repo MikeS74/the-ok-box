19d4f2ea1437bc079c7ad65aec3cc4e3a6483cb6
MikeS74 <mls@michaelleesanchez.com>
2018-12-01 16:23:20 -0800

    3 audio buttons working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-var x = document.getElementById("myAudio"); 
+var a = document.getElementById("audio1");
 
-function playAudio() { 
-    x.play(); 
+function playAudio1() {
+    a.play();
+}
+
+var b = document.getElementById("audio2");
+
+function playAudio2() {
+    b.play();
+}
+
+var c = document.getElementById("audio3");
+
+function playAudio3() {
+    c.play();
 }
