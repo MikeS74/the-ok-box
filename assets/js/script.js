5a7a0633796c2377ad374e6f42711ff66e016c94
MikeS74 <mls@michaelleesanchez.com>
2018-12-05 21:05:05 -0800

    audio retrigger added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
+// var audio = new Audio('audio_file.mp3');
+// audio.play();
+
 //Function for audio onclick
 function playAudio(val) {
     //Storing ID in a variable
     var a = document.getElementById("audio" + val);
+    a.currentTime = 0;
     a.play();
 }
