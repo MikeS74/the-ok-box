20234409667a1d85f9c9598e5cdc25840f7a9b2e
MikeS74 <mls@michaelleesanchez.com>
2018-12-05 18:38:03 -0800

    new playAudio function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,6 @@
-//Storing ID in a variable
-var a = document.getElementById("audio1");
-
 //Function for audio onclick
-function playAudio1() {
+function playAudio(val) {
+    //Storing ID in a variable
+    var a = document.getElementById("audio" + val);
     a.play();
-}
-
-var b = document.getElementById("audio2");
-
-function playAudio2() {
-    b.play();
-}
-
-var c = document.getElementById("audio3");
-
-function playAudio3() {
-    c.play();
 }
